7e07c6f92451e79be7bf387ad1ea35ca16b2e1ee
Andrew Jeschke <amjeschke@hotmail.com>
2025-11-19 21:54:46 -0600

    Simple hangman CLI game completed

---
+++ hangman-project/game.rb
@@ -0,0 +1,67 @@
+class Game
+    attr_accessor :game_over, :wrong_guesses, :word_list, :word, :user_word
+
+    def initialize()
+        @game_over = false
+        @wrong_guesses = Array.new()
+        @word_list = File.read('words.txt').split("\n")
+        @word = self.generate_word
+        @user_word = "_" * @word.length
+    end
+
+    def generate_word()
+        return @word_list[rand(0..@word_list.length - 1)]
+    end
+
+    def is_game_over?(guess)
+        check_guess(guess)
+
+        if @user_word == @word
+            @game_over = true
+            puts "You win"
+        elsif @wrong_guesses.length >= 6
+            @game_over = true
+            puts "You lose. The word was #{@word}. Try again?"
+        end
+        puts "Current word: #{@user_word}"
+        self.print_output(@wrong_guesses.length)
+    end
+
+    def check_guess(charguess)
+        if @word.include?(charguess)
+            @word.chars.each_with_index do |char, index|
+                if char == charguess
+                    @user_word[index] = char
+                end
+            end
+        else
+            @wrong_guesses.push(charguess)
+            puts "Guess ##{@wrong_guesses.length} incorrect. Try again."
+            puts "Wrong guesses: " + @wrong_guesses.join(', ')
+        end
+    end
+
+    def print_output(wrong_guesses)
+        case wrong_guesses
+        when 1
+            puts "  O  "
+        when 2
+            puts "  O  "
+            puts "  |  "
+        when 3
+            puts "  O  "
+            puts " /|  "
+        when 4
+            puts "  O  "
+            puts " /|\\ "
+        when 5
+            puts "  O  "
+            puts " /|\\ "
+            puts " /   "
+        else
+            puts "  O  "
+            puts " /|\\ "
+            puts " / \\ "
+        end
+    end
+end
---
+++ hangman-project/main.rb
@@ -0,0 +1,13 @@
+require_relative 'game'
+
+game = Game.new()
+
+puts "Welcome to Hangman!"
+puts "Current word: " + ("_" * game.word.length)
+puts game.word
+
+while !game.game_over
+    puts "Enter your guess:"
+    guess = gets.chomp.downcase
+    game.is_game_over?(guess)
+end
